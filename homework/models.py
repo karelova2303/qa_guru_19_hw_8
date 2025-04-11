71d16efc111e7d3f2d6438debc318cbdc24db922
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-04-11 23:55:15 +0300

    Добавление классов и методов в models

---
+++ homework/models.py
@@ -0,0 +1,95 @@
+class Product:
+    """
+    Класс продукта
+    """
+    name: str
+    price: float
+    description: str
+    quantity: int
+
+    def __init__(self, name, price, description, quantity):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.quantity = quantity
+
+    def check_quantity(self, quantity) -> bool:
+        """
+        TODO Верните True если количество продукта больше или равно запрашиваемому
+            и False в обратном случае
+        """
+        return self.quantity >= quantity
+
+    def buy(self, quantity):
+        """
+        TODO реализуйте метод покупки
+            Проверьте количество продукта используя метод check_quantity
+            Если продуктов не хватает, то выбросите исключение ValueError
+        """
+        if self.check_quantity(quantity):
+            self.quantity -= quantity
+        else:
+            raise ValueError('Данного продукта нет в нужном количестве')
+
+    def __hash__(self):
+        return hash(self.name + self.description)
+
+
+class Cart:
+    """
+    Класс корзины. В нем хранятся продукты, которые пользователь хочет купить.
+    TODO реализуйте все методы класса
+    """
+
+    # Словарь продуктов и их количество в корзине
+    products: dict[Product, int]
+
+    def __init__(self):
+        # По-умолчанию корзина пустая
+        self.products = {}
+
+    def add_product(self, product: Product, buy_count=1):
+        """
+        Метод добавления продукта в корзину.
+        Если продукт уже есть в корзине, то увеличиваем количество
+        """
+        if product.check_quantity(buy_count) and buy_count > 0:
+            if product in self.products:
+                self.products[product] += buy_count
+            else:
+                self.products[product] = buy_count
+        else:
+            raise ValueError('Данного продукта нет в нужном количестве')
+
+    def remove_product(self, product: Product, remove_count=None):
+        """
+        Метод удаления продукта из корзины.
+        Если remove_count не передан, то удаляется вся позиция
+        Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
+        """
+        if self.products.get(product):
+            if not remove_count or (remove_count > self.products[product]):
+                self.products.pop(product)
+            else:
+                self.products[product] -= remove_count
+        else:
+            raise ValueError('Данного продукта нет в корзине')
+
+    def clear(self):
+        self.products.clear()
+
+    def get_total_price(self) -> float:
+        total_price = 0
+        for key in self.products.keys():
+            total_price += key.price * self.products.get(key)
+        return total_price
+
+    def buy(self):
+        """
+        Метод покупки.
+        Учтите, что товаров может не хватать на складе.
+        В этом случае нужно выбросить исключение ValueError
+        """
+        for product in self.products.keys():
+            product.buy(self.products.get(product))
+        self.clear()
